85213aa168951e1a3319b4c32782021e8eb82743
alfchaval <alfchaval@hotmail.com>
2018-01-12 14:42:17 +0100

    12

--- app/src/main/java/com/example/usuario/navigatordrawer/MainActivity.java
+++ app/src/main/java/com/example/usuario/navigatordrawer/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.usuario.navigatordrawer;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,16 +13,18 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentOne.OnFragmentInteractionListener, FragmentTwo.OnFragmentInteractionListener {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        drawerLayout = findViewById(R.id.drawer_layout);
         toolbar = findViewById(R.id.toolbar);
         navigationView = findViewById(R.id.navigation_view);
         setSupportActionBar(toolbar);
@@ -37,19 +42,26 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_home:
-                        Log.d("navigatioDrawer", "Se ha pulsado la opción home");
+                        Log.d("navigationDrawer", "Se ha pulsado la opción home");
                         break;
                     case R.id.action_dependency:
-                        Log.d("navigatioDrawer", "Se ha pulsado la opción dependency");
+                        Log.d("navigationDrawer", "Se ha pulsado la opción dependency");
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.flContent, new FragmentOne());
+                        fragmentTransaction.commit();
                         break;
                     case R.id.action_sector:
-                        Log.d("navigatioDrawer", "Se ha pulsado la opción sector");
+                        Log.d("navigationDrawer", "Se ha pulsado la opción sector");
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.flContent, new FragmentTwo());
+                        fragmentTransaction.commit();
                         break;
                     case R.id.action_settings:
-                        Log.d("navigatioDrawer", "Se ha pulsado la opción settings");
+                        Log.d("navigationDrawer", "Se ha pulsado la opción settings");
+                        startActivity(new Intent(MainActivity.this, SettingActivity.class));
                         break;
                     case R.id.action_help:
-                        Log.d("navigatioDrawer", "Se ha pulsado la opción help");
+                        Log.d("navigationDrawer", "Se ha pulsado la opción help");
                         break;
                 }
                 item.setChecked(true);
@@ -77,5 +89,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/example/usuario/navigatordrawer/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.usuario.navigatordrawer;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.general_settings);
+    }
+}
